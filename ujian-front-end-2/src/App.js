d053440953e405d38bbf7a0a374067e7a027dd21
pupuPuwup <pandubelajarcoding@gmail.com>
2018-03-07 16:17:01 +0700

    update 3

--- ujian-front-end-2/src/App.js
+++ ujian-front-end-2/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Form from './components/Form';
+import './styles/Style.css'
 
 class App extends Component {
   constructor(){
@@ -11,19 +12,21 @@ class App extends Component {
     }
   }
   getData = team => 
-    axios.get('https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?t=${team}')
+    axios.get(`https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?t=${team}`)
     .then(res => this.setState({data: res.data.player}))
     .catch(err => console.log(err))
   render = () => 
-    <div id="app">
+  <div className="app">
+    <div className="panel-body">
       <div className="form-group">
-        <h1>{`Daftar Pemain ${this.state.search}`}</h1>
-        <input onChange={() => this.setState({search: this.refs.search.value})}className="form-control" type="text" ref="search"/>
+        <h1 className="huruf">{`Daftar Pemain ${this.state.search}`}</h1>
+        <input onChange={() => this.setState({search: this.refs.search.value})} className="col-md-6" type="text" ref="search"/>&nbsp;&nbsp;&nbsp;
         <button onClick={() => this.getData(this.refs.search.value)} className="btn btn-success">Lihat Daftar</button>
       </div>
       <div id="display">
-        {this.state.data.map(x => <List key={x.idPlayer} {...x}/>)}
+        {this.state.data.map(x => <Form key={x.idPlayer} {...x}/>)}
       </div>
+    </div>
   </div>
 }
 
--- ujian-front-end-2/src/components/Form.js
+++ ujian-front-end-2/src/components/Form.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import '../styles/Style.css'
 
-const List = props =>
-    <div className="list-container">
-        <h4 className="card text-white bg-primary">{`${props.strPlayer} (${props.strPosition})`}</h4>
-        <div>
-            <img src={props.strThumb} alt={props.strPlayer}/>
-            <p>{props.strDescriptionEN}</p>
+const Form = props =>
+    <div className="container">
+        <div className="panel-group">
+            <div className="panel panel-primary">
+                <div className="panel-heading">
+                    <h4 className="panel-title">{`${props.strPlayer} (${props.strPosition})`}</h4>
+                    <div className="panel-body">
+                        <img className="gambar" src={props.strThumb} alt={props.strPlayer}/>
+                        <p>{props.strDescriptionEN}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-
-export default List;
+export default Form;
